eb5de2b6b355baad32de884449c951df42d7a92f
ACRamirez1 <caramirez711@gmail.com>
2023-03-28 15:08:35 -0500

    Oh, CRUD

--- controllers/users.js
+++ controllers/users.js
@@ -1,60 +1,223 @@
 const mysql = require('mysql')
-const pool = require('../sql/connection')
+const db = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
-  pool.query("SELECT * FROM users", (err, rows) => {
-    if (err) return handleSQLError(res, err)
-    return res.json(rows);
+  // let sql = "select * from users u join usersAddress ua on u.id = ua.user_id join usersContact uc on u.id = uc.user_id";
+  
+
+  //another way to format with concat
+let sql ="select * from users u "
+sql += "join usersAddress ua on u.id = ua.user_id "
+sql += "join usersContact uc on u.id = uc.user_id"
+
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log("error description", err)
+      res.sendStatus(500)
+    } else {
+      res.json(rows);
+    }
   })
 }
 
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  let id = req.params.id;
+  let params = [id];
 
-  pool.query(sql, (err, rows) => {
-    if (err) return handleSQLError(res, err)
-    return res.json(rows);
+  // let id = req.params.id;
+  // let first = req.body.first_name;
+  // let phone = req.body.phone;
+  // let params = [id, first, phone];
+
+// this is a paramitized sequence when you begin to include other things like first name, phone, etc..!! 
+  let sql ="select * from users u "
+sql += "join usersAddress ua on u.id = ua.user_id "
+sql += "join usersContact uc on u.id = uc.user_id "
+sql += "and u.id = ?";  // +id works but BAD!!! what if the user enters 236; drop table users; //sql injection
+// sql += "and u.id = ? and first_name = ? and phone1 = ?";
+  
+db.query(sql, params, (err, rows) => {
+    if (err) {
+      console.log("getUserById query failed", err);
+      res.sendStatus(400);
+    } else {
+      res.json(rows);
+    }
   })
 }
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
+  // SYNC use promises (async/await)
+  // FIRST QUERY
+  let first = req.body.first_name;
+  let last = req.body.last_name;
+
+  let params = [first, last];
+  let sql = "insert into users (first_name, last_name) values (?, ?)";
+
+  let results;
+
+  try {
+    results = await db.querySync(sql, params);
+  } catch(err){
+  console.log("insert user query failed ", err)
+  res.sendStatus(500);
+  return; // if this query didn't work, stop
+}
+// res.json(results)
+  let id =  results.insertId;
+// SECOND QUERY
+  let address = req.body.address;
+  let city = req.body.city;
+  let county = req.body.county;
+  let state = req.body.state;
+  let zip = req.body.zip;
+
+  params = [id, address, city, county, state, zip];
+
+  sql = "insert into usersAddress (user_id, address, city, county, state, zip) values (?, ?, ?, ?, ?, ?)"
+
+  try {
+    results = await db.querySync(sql, params);
+  } catch(err){
+  console.log("insert usersAddress query failed ", err)
+  res.sendStatus(500);
+  return; // if this query didn't work, stop
+}
+
+// THIRD QUERY
+let phone1 = req.body.phone1;
+let phone2 = req.body.phone2;
+let email = req.body.email;
+
+params = [id, phone1, phone2, email];
+
+sql = "insert into usersContact (user_id, phone1, phone2, email) values (?, ?, ?, ?)"
+
+try {
+  results = await db.querySync(sql, params);
+} catch(err){
+console.log("insert usersContact query failed ", err)
+res.sendStatus(500);
+return; // if this query didn't work, stop
+}
+
+}
+
+
+const createUserCallbackHell = (req, res) => {
+  // POST
+  // ASYNC NESTED CALLBACKS
   // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  // INSERT IN usersAddress ALL FIELDS, SO I'LL NEED THE USERS.ID TO DO THIS INSERT
+  // INSERT IN usersContact ALL FIELDS, SO I'LL NEED THE USERS.ID TO DO THIS INSERT
 
-  pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
-    return res.json({ newId: results.insertId });
-  })
-}
+  let sql = "insert into users (first_name, last_name) values (?, ?)";
+  let first = req.body.first_name;
+  let last = req.body.last_name;
+  
+  let params = [first, last];
+
+  // FIRST QUERY
+  db.query(sql, params, (err, rows) => {
+    if (err){
+      console.log('error message ', err)
+      res.sendStatus(500) // or whatever is appropriate
+    } else {
+      // SECOND QUERY
+      sql = "select MAX(id) as id from users where first_name = ? and last_name = ?"
+      db.query(sql, params, (err, rows) => {
+        if(err) {
+          //do the err stuff
+          console.log('get id query failed ', err)
+          res.sendStatus(500) // or whatever is appropriate
+        } else {
+          // res.json(rows[0])
+          let id = rows[0].id; //NOW I have the id from the users table
+          let address = req.body.address;
+          let city = req.body.city;
+          let county = req.body.county;
+          let state = req.body.state;
+          let zip = req.body.zip;
+
+          params = [id, address, city, county, state, zip];
+
+          sql = "insert into usersAddress (user_id, address, city, county, state, zip) values (?, ?, ?, ?, ?, ?)"
+
+          db.query(sql, params, (err, rows) => {
+            if (err) {
+              console.log("insert address query failed ", err) 
+              res.sendStatus(500);
+            } else {
+              // res.json(rows)
+              // FOURTH QUERY
+              let phone1 = req.body.phone1;
+              let phone2 = req.body.phone2;
+              let email = req.body.email;
+              
+              params = [id, phone1, phone2, email];
+
+              sql = "insert into usersContact (user_id, phone1, phone2, email) values (?, ?, ?, ?)"
+
+              db.query(sql, params, (err, rows) => {
+                if(err) {
+                  console.log("insert contact query failed ", err)
+                  res.sendStatus(500);
+                } else {
+                  // res.json(rows)
+                  res.sendStatus(200);
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+})
+};
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  id = req.params.id;
+  let first = req.body.first_name;
+  let last = req.body.last_name;
+  let params = [id, first, last];
+  let sql = "update users set first_name = ? last_name = ? where id = ?"
+  
+if(!first) {
+  res.sendStatus(400);
+  return;
+}
 
-  pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
-    return res.status(204).json();
+  
+  db.query(sql, params, (err, rows) => {
+    if (err) {
+      console.log("getAllUsers query failed", err);
+      res.sendStatus(400);
+    } else {
+      res.json(rows);
+    }
   })
 }
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let sql = ""
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  let first = req.params.first_name; // /user/bob
+  let params = [first];
 
-  pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
-    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
+
+  let sql = "delete from users where first_name = ?"
+
+  db.query(sql, params, (err, rows) => {
+    if (err) {
+      console.log("deleteUserByFirstName query failed", err)
+    } else {
+    return res.json({ message: `Deleted ${rows.affectedRows} user(s)` });
+    }
   })
 }
 
--- sql/connection.js
+++ sql/connection.js
@@ -1,24 +1,90 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
+require("dotenv").config();
 
-class Connection {
-  constructor() {
-    if (!this.pool) {
-      console.log('creating connection...')
-      this.pool = mysql.createPool({
-        connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      })
 
-      return this.pool
-    }
-
-    return this.pool
+let connection = mysql.createConnection(
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
   }
+);
+
+connection.connect();
+
+//basic promise wrapper if you want to just convert a callback to a promise
+connection.queryPromise = (sql, params) => {
+  return new Promise ( (reslove, reject) => {
+    connection.query(sql, params, (err, rows) => {
+      if (err){
+        reject(err);
+      } else {
+        reslove(rows);
+      }
+    })
+  })
+};
+
+
+// go farther and process the results of the promies and return the results
+// helper function middleware instead of installing and learning another promise module
+connection.querySync = async (sql, params) => {
+  let promise = new Promise ( (resolve, reject) => {
+    console.log("Executing query: ", sql);
+    connection.query(sql, params, (err, results) => {
+    if (err) {
+      console.log("rejecting");
+      return reject(err);
+    } else {
+      console.log("resolving");
+      return resolve(results);
+    }
+  })
+})
+let results = await promise.then ( (results) =>{
+  console.log("results ", results)
+  return results;
+}).catch( (err) => {
+  throw err;
+})
+return results;
+
 }
 
-const instance = new Connection()
 
-module.exports = instance;
+
+
+//test to see if the connection was made
+connection.query("select now()", (err,rows) => {
+  if(err) {
+    console.log("not connected", err);
+  } else {
+    console.log("connected", rows)
+  }
+});
+
+module.exports = connection;
+
+// class Connection {
+//   constructor() {
+//     if (!this.pool) {
+//       console.log("creating connection...");
+//       this.pool = mysql.createPool({
+//         connectionLimit: 100,
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PWD,
+//         database: process.env.DB_NAME,
+//       });
+
+//       return this.pool;
+//     }
+
+//     return this.pool;
+//   }
+// }
+
+// const instance = new Connection();
+
+// module.exports = instance;
